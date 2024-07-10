eb99aa67b628aa7512905e6c2ef30ef9125bd6a0
AGINSHAJU <104190720+AGINSHAJU@users.noreply.github.com>
2024-07-10 10:55:52 +0530

    Update urls.py

--- myproject/urls.py
+++ myproject/urls.py
@@ -4,7 +4,7 @@ from myapp import views
 
 urlpatterns = [
    path('admin/', admin.site.urls),
-   path('',views.home,name="home"),
+   path('',views.index,name="index"),
    path('whether/', views.whether, name="whether"), 
    path('about/', views.about, name='about'),
 
