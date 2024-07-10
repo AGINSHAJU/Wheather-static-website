3b68c5ae8f4490d670cf7dbec0d69cef64d06a1b
AGINSHAJU <104190720+AGINSHAJU@users.noreply.github.com>
2024-07-10 10:55:18 +0530

    Update urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home, name='index'),
     path('weather/', views.weather, name='weather'),  # Updated to match the view function name
     path('about/', views.about, name='about'),
 
